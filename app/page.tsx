e727c578b3b25c077f054104bffe9aca7274d352
jojazpeitia <jojazpeitia@gmail.com>
2023-08-22 19:46:23 -0500

    added shadcn slider + control icons

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,27 @@
 import Image from 'next/image'
-import {MdClose, MdRemove, MdOutlineHome, MdCheckBoxOutlineBlank} from 'react-icons/md'
-
+import {MdClose, MdRemove, MdOutlineHome, MdCheckBoxOutlineBlank, MdPlayCircle, MdSkipNext, MdSkipPrevious, MdShuffle, MdLoop} from 'react-icons/md'
+import { Slider } from "@/components/ui/slider"
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className='bg-gray-100 w-96 h-96 shadow-3xl rounded-xl'>
-        <div className='bg-white flex flex-row justify-between'>
+    <main className="flex min-h-screen flex-col items-center justify-between p-64">
+      <div className=' bg-gray-100 w-96 h-80 shadow-3xl rounded-md flex flex-col justify-between'>
+        <div className='bg-white flex flex-row justify-between p-2'>
           <MdOutlineHome/>
-          <div className='flex flex-row'>
+          <div className='flex flex-row space-x-2'>
             <MdRemove/>
             <MdCheckBoxOutlineBlank/>
             <MdClose/>
+          </div>
+        </div>
+        <div className='bg-white'>
+          <Slider defaultValue={[33]} max={100} step={1} />
+          <div className='flex items-center justify-center space-x-4 my-2'>
+            <MdShuffle/>
+            <MdSkipPrevious/>
+            <MdPlayCircle/>
+            <MdSkipNext/>
+            <MdLoop/>
           </div>
         </div>
       </div>
